b3b7af35af2d1e6b078a40cbb9652e12dc148ed8
Evgeny <jenyamyasnikov@gmail.com>
2025-11-11 15:50:42 +0800

    refactor: 1

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,20 +1,9 @@
-import { Link } from '../link/Link'
 import s from './footer.module.css'
 
 export const Footer = () => {
 	return (
 		<footer className={s.footer} role='contentinfo'>
-			<p className={s.footerText}>
-				Проект выполнен в рамках стажировки{' '}
-				<Link
-					href={'https://preax.ru/'}
-					aria-label='Перейти на сайт PREAX (откроется в новой вкладке)'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					PREAX
-				</Link>
-			</p>
+			<p className={s.footerText}>Проект TaskanaApp</p>
 		</footer>
 	)
 }
